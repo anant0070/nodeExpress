ea97ec4c56da1ba9e7cfbdab53fc6275773da63d
Anant Chaudhary <anant0070.be20@chitkara.edu.in>
2023-05-14 22:45:11 +0530

    engine

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 export const connectDB = () => {
 mongoose.connect(process.env.MONGO_URI, {
     dbName: "backendapi",
-}).then(c => console.log("Databse connected"))
+}).then((c) => console.log(`Databse connected with ${c.connection.host}`))
     .catch(e => console.log(e));
 }
